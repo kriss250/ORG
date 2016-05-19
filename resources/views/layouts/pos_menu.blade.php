bca95e70b53c06e740dfebb1f29b8d43ba8bd4a0
Hakizimana Christian <info@kris.rw>
2016-05-19 07:24:04 +0200

    added freec co. menu

--- resources/views/layouts/pos_menu.blade.php
+++ resources/views/layouts/pos_menu.blade.php
@@ -5,6 +5,8 @@
     <li><a href="{{ action("BackofficeReportController@index",'cashierShift') }}">Cashier Shift</a> </li>
     <li><a href="{{ action("BackofficeReportController@index",'cashierBills') }}">Cashier Bills</a> </li>
     <li><a href="{{ action("BackofficeReportController@index",'productSales') }}">Products Sales(Control)</a> </li>
+    
+    <li><a href="{{ action("BackofficeReportController@index",'offtariffBills') }}">Off Tarriff (Free Co.)</a> </li>
     <li><a href="{{ action("BackofficeReportController@index",'roomPosts') }}">Room Posts</a> </li>
     <li><a href="{{ action("BackofficeReportController@index",'debts') }}">Debts</a> </li>
     <li><a href="{{ action("BackofficeReportController@index",'cancelledBills') }}">Cancelled Bills</a> </li>
