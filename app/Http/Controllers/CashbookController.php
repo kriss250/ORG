093d56beded65ea77d4ce4403be7c69bda980d06
Hakizimana Christian <info@kris.rw>
2016-05-23 22:23:06 +0200

    casshbook fix

--- app/Http/Controllers/CashbookController.php
+++ app/Http/Controllers/CashbookController.php
@@ -59,13 +59,13 @@ class CashbookController extends Controller
      */
     public function show($id)
     {
-        
-      
+
+
         $cashbook = \DB::connection("mysql_backoffice")->select("select cashbookid,cashbook_name,balance from cash_book where   cashbookid=?",[$id])[0];
 
         $params = [$id];
         $where = "";
-        
+
         if(isset($_GET['date']) && strlen($_GET['date'])>2)
         {
             $d = explode('/', $_GET['date']);
@@ -75,24 +75,34 @@ class CashbookController extends Controller
             $params = [$id,$date];
         }else{
             $dv_date = \Session::get('date');
-            
+
             $params = [$id,\ORG\Dates::$RESTODATE];
-            
+
             if(strlen($dv_date)>6)
             {
                 $params = [$id,$dv_date];
             }
-            
+
             $where = " and date(cashbook_transactions.date)=?";
         }
-        
+
         $date = new \DateTime($params[1]);
         $date->sub(new \DateInterval('P1D'));
         $prev_date = $date->format('Y-m-d');
-        
-        $prev_amount = \DB::connection("mysql_backoffice")->select("SELECT new_balance FROM cashbook_transactions where deleted=0 and date(date)<=? and cashbook_id=? and recorded=0 order by transactionid desc limit 1",[$prev_date,$id]);
 
-        $initial = isset($prev_amount[0]->new_balance) ? $prev_amount[0]->new_balance : 0 ;
+        $in= 0;
+        $out = 0;
+
+        $in_out = \DB::connection("mysql_backoffice")->select("select type,sum(amount) as am from cashbook_transactions where deleted=0 and date(date)='2016-05-22' and cashbook_id=? group by type",[ $prev_date, $id]);
+
+        foreach($in_out as $xc)
+        {
+            $in = $xc->type == "IN" ? $xc->amt : 0;
+            $out = $xc->type == "OUT" ? $xc->amt : 0;
+        }
+
+        $initial = $in - $out;
+
         $transactions = \DB::connection("mysql_backoffice")->select("select transactionid,new_balance,type,amount,motif,username,cashbook_transactions.date from cashbook_transactions join org_pos.users on org_pos.users.id = user_id where cancelled=0 and cashbook_id=? and deleted=0 $where order by transactionid asc",$params);
 
         return \View::make("Backoffice.openCashbook",['cashbook'=>$cashbook,"transactions"=>$transactions,"initial"=>$initial]);
@@ -131,5 +141,5 @@ class CashbookController extends Controller
     {
         //
     }
-    
+
 }
