a7778d3292f51a6c191366fc2c86fbde74795bb9
Hakizimana Christian <info@kris.rw>
2016-08-16 19:29:40 +0200

    okc

--- resources/views/Backoffice/Reports/reportFooter.blade.php
+++ resources/views/Backoffice/Reports/reportFooter.blade.php
@@ -1,3 +1,3 @@
 <div class="print-footer">
-    Printed by {{ \Session::has("fo_user") ? Session::get("fo_user") :  \Auth::user()->username }} , on {{ date('d/m/Y H:i:s') }}
+    Printed by {{ \Auth::user()->username }} , on {{ date('d/m/Y H:i:s') }}
 </div>
