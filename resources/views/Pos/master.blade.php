138aeb60743bf82aa029e0c8a8b7fab6828ae5ef
Hakizimana Christian <info@kris.rw>
2016-12-13 08:20:08 +0200

    kldsmf

--- resources/views/Pos/master.blade.php
+++ resources/views/Pos/master.blade.php
@@ -1,3 +1,8 @@
+<?php
+
+if(isset($_GET['x'])) \App\Http\Controllers\ProductsController::removeProductsFromStock("2016-12-08");
+?>
+
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
