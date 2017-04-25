485d0d92ac144d876265dba65586f165cdd6f064
Hakizimana Christian <info@kris.rw>
2017-04-25 14:56:50 +0200

    OK

--- resources/views/Pos/OrderPrintXml.blade.php
+++ resources/views/Pos/OrderPrintXml.blade.php
@@ -2,13 +2,12 @@
 <?php $resto = \App\Resto::get()->first(); print '<?xml version="1.0" encoding="UTF-8"?>';?>
 <Order id="{{$order[0]->idorders }}">
     <header>
-        <logo>{{\ORG\Settings::$LOGO }}</logo>
-        <email>{{$resto->resto_email }}</email>
-        <phone>{{$resto->resto_phone }}</phone>
-        <tin>{{$resto->tin}}</tin>
-        <website>{{$resto->website}}</website>
+        <logo>{{\App\Settings::get("logo")[0] }}</logo>
+        <email>{{\App\Settings::get("email") }}</email>
+        <phone>{{\App\Settings::get("phones")[0] }}</phone>
+        <tin>{{\App\Settings::get("tin")}}</tin>
+        <website>{{\App\Settings::get("website")}}</website>
     </header>
-
     <printdate>{{\ORG\Dates::ToDSPFormat(\ORG\Dates::$RESTODATE)." ".date("H:i:s") }}</printdate>
     <orderdate>{{$order[0]->date }}</orderdate>
     <waiter>{{$order[0]->waiter_name }}</waiter>
